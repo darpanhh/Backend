4b1e7ba6e10bdda2a80b30e2afe98c4fb1a738ce
darpanhh <darpangiri5@gmail.com>
2025-12-10 23:56:16 +0545

    Initial commit

---
+++ Day 1/math.js
@@ -0,0 +1,28 @@
+// const sum = (a,b) => a+b;
+// const multiply = (a,b) => a*b;
+// const g =9.8;
+// const PI=3.14;
+
+// module.exports.sum = (a,b) => a+b;
+// module.exports.multiply = (a,b) => a*b;
+// module.exports.g=9.8;
+// module.exports.PI=3.14;
+
+exports.sum = (a,b) => a+b;
+exports.multiply = (a,b) => a*b;
+exports.g=9.8;
+exports.PI=3.14;
+// let obj = {
+//     sum:sum,
+//     multiply:multiply,
+//     g:g,
+//     PI:PI
+// }
+
+// module.exports = obj;
+// module.exports={
+//     sum:sum,
+//     multiply:multiply,
+//     g:g,
+//     PI:PI
+// }
---
+++ Day 1/script.js
@@ -0,0 +1,20 @@
+// let n =5;
+// for(let i=0;i<n;i++){
+//     console.log("Hello",i);
+// }
+
+// let args = process.argv;
+
+// for(let i = 2;i<args.length;i++){
+//     console.log("Hello to ",args[i])
+// }
+// console.log(process.argv)
+// const math = require('./math')
+
+// console.log(math)
+// console.log(math.sum(2,2))
+// console.log(math.PI)
+const info = require('./Fruits');
+console.log(info);
+console.log(info[0]);
+console.log(info[0].name);
