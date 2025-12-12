b94561f08d04b7a8a93e9213073082c31d99e707
darpanhh <darpangiri5@gmail.com>
2025-12-12 16:51:26 +0545

    Beggining of RESTful apis

---
+++ Day 5(RESTful APIs)/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require('path');
+
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,"public"));
+
+app.set(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.send("server working well");
+})
+
+
+app.listen(port,()=>{
+    console.log(`app is listening to the port ${port}`);
+})
