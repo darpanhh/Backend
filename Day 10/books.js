598089d2b94aa820e47ac2d05f7f4db6598a56cb
darpanhh <darpangiri5@gmail.com>
2025-12-20 22:38:39 +0545

    Mongoose completed

---
+++ Day 10/books.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+main()
+.then(()=>{
+    console.log("Connection successful");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/amazon");
+}
+
+const bookSchema = new mongoose.Schema({
+    title:{
+        type:"String",
+        required:true,
+        maxLength:20
+    },
+    author:{
+        type:"String",
+    },
+    price:{
+        type:"Number",
+        min:[1,'price is too low for Amazon selling'],
+    },
+    discount:{
+        type:"Number",
+        default:0
+    },
+    category:{
+        type:"String",
+        enum:['fiction','non-fiction']
+    },
+    genre:[String]
+})
+
+const Book = mongoose.model("Book",bookSchema);
+
+Book.findByIdAndUpdate(
+    '6946d134320358d7ca66d981',
+    {price:-100},
+    {runValidators:true}
+).then(
+    (res)=>{console.log(res);      
+    }
+).catch(err=>{console.log(err.errors.price.properties.message);})
+
+// let book1 = new Book({
+//     title:"Marvel comics v2",
+//     // author:"Darpan Giri",
+//     price:500,
+//     category:"fiction",
+//     genre:['comics','superheroes','fiction']
+// });
+
+// book1.save().then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
--- Day 10/index.js
+++ Day 10/index.js
@@ -18,20 +18,85 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User",userSchema);
 
-User.insertMany([
-    {name:"Tony",email:"tony@gmail.com",age:50},
-    {name:"Bruce",email:"bruce@gmail.com",age:47},
-    {name:"Peter",email:"peter@gmail.com",age:30},
-]).then((data)=>{
-    console.log(data);
+// User.deleteOne({name:"Bruce"}).then((res)=>{
+//     console.log(res);
+// })
+// .catch(err=>{
+//     console.log(err);
+// })
+// User.deleteMany({age:48}).then((res)=>{
+//     console.log(res);
+// })
+// .catch(err=>{
+//     console.log(err);
+// })
+// User.findByIdAndDelete("69452efaca5623d5240b0998").then((res)=>{
+//     console.log(res);
+// })
+// .catch(err=>{
+//     console.log(err);
+// })
+User.findOneAndDelete({name:"Tony"}).then((res)=>{
+    console.log(res);
 })
-.catch((err)=>{
+.catch(err=>{
     console.log(err);
 })
 
+// User.insertMany([
+//     {name:"Tony",email:"tony@gmail.com",age:50},
+//     {name:"Bruce",email:"bruce@gmail.com",age:47},
+//     {name:"Peter",email:"peter@gmail.com",age:30},
+// ]).then((data)=>{
+//     console.log(data);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
 
+//  User.find({})
+//  .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
 
-
+// User.find({age:{$gt:47}})
+//  .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+// User.findById('69452cf2931f3780f241bd8d')
+//  .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+// User.updateOne({name:'Adam'},{age:49})
+// .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+// User.findOneAndUpdate({name:"Bruce"},{age:42},{new:true})
+// .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
+// User.findByIdAndUpdate('69452efaca5623d5240b0997',{age:64},{new:true})
+// .then((res)=>{
+//     console.log(res);
+// })
+// .catch((err)=>{
+//     console.log(err);
+// });
 
 // const user2 = new User(
 //     {
