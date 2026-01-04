4190a30411c6d157bd2cbc52ab08aaa5b371037d
darpanhh <darpangiri5@gmail.com>
2026-01-04 23:27:14 +0545

    CRUD operation

---
+++ Project/app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
+const path = require('path');
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.send('Hi,I am root');
+});
+
+//Index Route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render('listings/index.ejs', { allListings });
+})
+//New Route
+app.get('/listings/new',(req,res)=>{
+    res.render('listings/new.ejs');
+}) 
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render('listings/show.ejs', { listing });
+})
+
+//Create Route
+app.post("/listings",async(req,res)=>{
+    // let {title,description,image,price,country,location} = req.body;
+    // let listing = req.body.listing;
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect('/listings');
+    // console.log(newListing);
+})
+
+
+// app.get('/testListing',async(req,res)=>{
+//     let sampleListing = new Listing({
+//         title:"My New Villa",
+//         description:"By the beach",
+//         price:1200,
+//         location:"Calangute, Goa",
+//         country:"India",
+//     });
+//     await sampleListing.save();
+//     console.log("Sample was saved");
+//     res.send('Successful testing');
+// });
+
+
+
+app.listen(8080, () => {
+    console.log("Server is listening to the port 8080");
+})
---
+++ Project/models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default: "https://images.pexels.com/photos/35171751/pexels-photo-35171751.jpeg",
+    set: (v) =>
+      v === ""
+        ? "https://images.pexels.com/photos/35171751/pexels-photo-35171751.jpeg"
+        : v
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
