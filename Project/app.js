9ee1a6d7bed9da55b5997aa7b5b32084eb0c2b9c
darpanhh <darpangiri5@gmail.com>
2026-02-03 23:48:20 +0545

    Signup added

--- Project/app.js
+++ Project/app.js
@@ -10,12 +10,17 @@ const ExpressError = require('./utils/ExpressError.js')
 const Joi = require('joi');
 // const {listingSchema,reviewSchema} = require('./schema.js')
 // const Review = require('./models/review.js');
-const listings = require('./routes/listing.js')
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const reviews = require('./routes/review.js')
+
+const listingRouter = require('./routes/listing.js')
+const reviewRouter = require('./routes/review.js')
+const userRouter = require('./routes/user.js')
+
 const session = require('express-session')
 const flash  = require('connect-flash');
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
 
 
 main()
@@ -59,6 +64,13 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -67,9 +79,20 @@ app.use((req,res,next)=>{
     next();
 })
 
-app.use('/listings',listings);
-app.use('/listings/:id/reviews',reviews);
+app.get('/demouser',async(req,res)=>{
+    let fakeUser = new User({
+        email:'student@gmail.com',
+        username:'delta-student'
+    });
+    let registeredUser = await User.register(fakeUser,'helloworld');
+    res.send(registeredUser);
+})
 
+
+
+app.use('/listings',listingRouter);
+app.use('/listings/:id/reviews',reviewRouter);
+app.use('/',userRouter);
 
 
 // app.get('/testListing',async(req,res)=>{
